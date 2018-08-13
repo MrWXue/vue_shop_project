4a18b8eaae108ca0c03bbda55a35aebd52d00afa
xuewei <1941733184@qq.com>
2018-08-13 15:19:11 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
+import ElementUI from 'element-ui'
+import axios from 'axios'
+Vue.use(ElementUI)
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 
 Vue.config.productionTip = false
 
